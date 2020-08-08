e4436f1f881d584ee6d5923ebb0cc33097e65144
Lewisecurity <matt@lewisecurity.com>
2020-08-08 15:12:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='vectrix',
     packages=['vectrix'],
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Vectrix Developer Python Package',
     author='Matthew Lewis',
