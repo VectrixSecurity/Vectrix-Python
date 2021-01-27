0650b8c11b268360a12eb0ba0c014ada4806375f
Lewisecurity <matt@lewisecurity.com>
2021-01-27 13:58:07 -0800

    Add coverage, tox, and modify setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,43 @@
-from distutils.core import setup
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+# Package Version
+VERSION = "0.2.1"
+
+
+def readme():
+    """print long description"""
+    with open('README.md') as f:
+        return f.read()
+
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+
 setup(
     name='vectrix',
     packages=['vectrix'],
-    version='0.2.1',
+    version=VERSION,
     license='MIT',
     description='Vectrix Developer Python Package',
+    long_description=readme(),
     author='Matthew Lewis',
     author_email='matthew.lewis@vectrix.io',
     url='https://github.com/VectrixSecurity/Vectrix-Python',
-    download_url='https://github.com/VectrixSecurity/Vectrix-Python/archive/v0.2.0.tar.gz',
     keywords=['vectrix', 'vectrixio', 'vectrix.io', 'vectrix module'],
     install_requires=[
         'requests==2.22.0',
@@ -26,4 +55,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
+    python_requires='>=3',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
